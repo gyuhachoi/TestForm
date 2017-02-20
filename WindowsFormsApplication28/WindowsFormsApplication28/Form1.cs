2b2c10e6e4b3cf886bb51ad39903a3707f3b6464
gyuhachoi <chlrbgk147@naver.com>
2017-02-20 14:24:24 +0900

    fist commit

---
+++ WindowsFormsApplication28/WindowsFormsApplication28/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication28
+{
+    public class Dummy
+    {
+        public int dummy1 = 0;
+    }
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Load += Form1_Load;
+        }
+
+        void Form1_Load(object sender, EventArgs e)
+        {
+            // struct, value
+            // class, reference
+            int value = 0;
+
+            ChangeValue(value);
+            Debug.WriteLine("value: " + value); // 0
+
+            Dummy dummy = new Dummy();
+            ChangeValue2(dummy);
+            Debug.WriteLine("dummy.dummy1: " + dummy.dummy1); // 10
+        }
+
+        public void ChangeValue(int param)
+        {
+            param = 10;
+        }
+
+        public void ChangeValue2(Dummy param)
+        {
+            param.dummy1 = 10;
+        }
+    }
+}
